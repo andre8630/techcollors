6e50da50799f5755c35b42e93c62c68e896ed2ce
andre8630 <andre8630@gmail.com>
2025-11-23 18:17:24 -0300

    rafactor on the status end-point and tests

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -2,8 +2,15 @@ describe("GET /api/v1/status", () => {
   describe("Anonymous user", () => {
     test("Current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
       expect(response.status).toBe(200);
+
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.dependencies.database.version).toEqual("17.5");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.current_connections).toEqual(1);
     });
   });
 });
