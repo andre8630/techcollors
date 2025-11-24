e0fffaa24fa268409be1bda22e55dc0665196a5e
andre8630 <andre8630@gmail.com>
2025-11-24 07:51:04 -0300

    add try catch and finaly on database.js

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,18 +1,24 @@
 import { Client } from "pg";
+let client;
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    client = new Client({
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      ssl: process.env.NODE_ENV === "development" ? false : true,
+    });
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch {
+    console.log("Erro de conexao com banco");
+  } finally {
+    await client.end();
+  }
 }
 
 const database = {
