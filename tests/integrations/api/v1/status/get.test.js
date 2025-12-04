2a8df3d08627b0bdc12df8f2399bed11f3f14845
andre8630 <andre8630@gmail.com>
2025-12-03 21:47:46 -0300

    feat: add status page, install swr and create a single test

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -20,5 +20,10 @@ describe("GET /api/v1/status", () => {
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
       expect(responseBody.dependencies.database.current_connections).toEqual(1);
     });
+    test("Current page status", async () => {
+      const response = await fetch("http://localhost:3000/status");
+
+      expect(response.status).toBe(200);
+    });
   });
 });
