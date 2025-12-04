b3cd924d7ea27064595a22bdbde92730d075ef37
andre8630 <andre8630@gmail.com>
2025-12-04 20:32:32 -0300

    fix: prettier error on ci

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -14,9 +14,7 @@ describe("GET /api/v1/status", () => {
       expect(response.status).toBe(200);
 
       expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-      expect(responseBody.dependencies.database.version).toEqual(
-        "17.5" || "16.9"
-      );
+      expect(responseBody.dependencies.database.version).toEqual("17.5");
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
       expect(responseBody.dependencies.database.current_connections).toEqual(1);
     });
