3bbbeec3e241463097173b4dd2c0bffc7b8b67ea
andre8630 <andre8630@gmail.com>
2025-12-08 16:09:48 -0300

    fix: trying fix server error before model user includes

--- src/models/migrator.js
+++ src/models/migrator.js
@@ -1,10 +1,10 @@
 import database from "src/infra/database";
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 
 const defaultMigrationOptions = {
   dryRun: true,
-  dir: join(process.cwd(), "src", "infra", "migrations"),
+  dir: resolve("src", "infra", "migrations"),
   direction: "up",
   verbose: true,
   migrationsTable: "pgmigrations",
